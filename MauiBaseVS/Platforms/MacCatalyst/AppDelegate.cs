90cd7de873451f0ecff6a08adc55ac2e084f90fd
Rick Van Dyk <rick@rickvandyk.com>
2022-10-19 10:32:13 -0600

    Add project files.

---
+++ MauiBaseVS/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiBaseVS;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
