457186afcaf6235e8f57f9032b79ea28af7e650e
Rick Van Dyk <rick@rickvandyk.com>
2022-10-21 16:58:32 -0600

    commit

---
+++ MauiBaseVS/Models/UserDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiBaseVS.Models
+{
+    public class UserDetails
+    {
+        public string Name { get; set; }
+        public string Location { get; set; }
+    }
+}
--- MauiBaseVS/ViewModels/MainViewModel.cs
+++ MauiBaseVS/ViewModels/MainViewModel.cs
@@ -1,12 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using MauiBaseVS.Models;
 
 namespace MauiBaseVS.ViewModels
 {
-    internal class MainViewModel
+    public class MainViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private List<UserDetails> _userDetailsList = new List<UserDetails>();
+
+        public List<UserDetails> UserDetailsList
+        {
+            get => _userDetailsList;
+
+            set
+            {
+                _userDetailsList = value;
+                OnPropertyChanged();
+            }
+            
+        }
+
+        private void OnPropertyChanged()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICommand SelectionChangedCommand => new Command<object>((obj) =>
+        {
+            var selectedItem = obj as UserDetails;
+        });
+
+        public MainViewModel()
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            List<UserDetails> detailsList = new List<UserDetails>();
+
+            detailsList.Add(new UserDetails { Name = "Rick", Location = "Colorado" });
+            detailsList.Add(new UserDetails { Name = "Bill", Location = "Iowa" });
+            detailsList.Add(new UserDetails { Name = "Tom", Location = "Nebraska" });
+            detailsList.Add(new UserDetails { Name = "William", Location = "Arizonia" });
+
+            UserDetailsList = detailsList;
+        }
     }
 }
